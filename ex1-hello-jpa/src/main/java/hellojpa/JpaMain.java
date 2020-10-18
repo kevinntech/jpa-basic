9caf80a9c7a0fead1a802525b8e6f4164ab54c46
kevinntech <kevinntech@nate.com>
2020-10-18 23:05:50 +0900

    영속성 관리 - 내부 동작 방식 까지

---
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,37 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+
+        tx.begin();
+
+        try{
+
+            Member member = em.find(Member.class , 150L);
+            member.setName("AAAAA");
+
+            em.clear();
+
+            Member member2 = em.find(Member.class, 150L);
+
+            System.out.println("================");
+            tx.commit();
+        }catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+
+        emf.close(); // 전체 애플리케이션이 끝나면 EntityManagerFactory도 닫는다.
+    }
+}
